ad4ce784bf85d2ea66a34390b84ad5ec09fada22
Noel-Garces08 <noel.garces08@gmail.com>
2020-11-26 13:55:35 -0700

    for loop exercises

---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * What are loops? A loop is used to execute a block of code several times
+ */
+
+// EXERCISES 
+
+/**
+ * Exercise One
+ * Title: Print all even numbers up to n (n being a number of your choice)
+ * Description: Using a for loop prinat all even number up to and including n. Don't include 0.
+ */
+const n = 0;
+
+/**
+ * Exercise Two
+ * Title: The even/odd reporter
+ * Description: Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd
+ * and report that to the console (e.g. "2 is even").
+ * */
+
+/**
+ * Exercise Three
+ * Title: Multiplication Tables
+ * Description: Write a for loop that will iterate from 0 to 10. For Each iteration of the loop, it will multiply the number by 9 and
+ * log the result (e.g. 2 * 9 = 18)
+ * Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total)
+ */
+
+/**
+ * Exercise Four
+ * Title: Sum of Two Arrays
+ * Description: Get the sum of two arrays
+ * Example: 
+    -> let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+    -> let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+    -> Example output: 
+    -> 276 + 351 = 627
+*/
+
+/**
+* Exercise Five
+* Title: Reverse it
+* Description: Using a for loopto  output the elements in reverse order.
+*/
+const elements = [43, 'what', 9, true, 'cannot', false, 'be', 3, true];
+
+
+/**
+ * Exercise Six
+ * Title: Child making
+ * Description: Given two arrays of integers. Add up each element in the same position and sum of each pair into child.
+ * Assume both arrays are the same length
+ */
+const mom = [4, 6, 7];
+const dad = [8, 1, 9];
+const child = [];
+
+
+/**
+ * Exercise Seven
+ * Title: isPalindrom
+ * Description: Write a program that will check if two strings are palindromes. A palindrome is a word that spells the same forward and backward.
+ */
+let word = 'racecar';
+
+
